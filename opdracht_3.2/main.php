ddafc44095f7c46ba2d9a836951a434d7adfb9fe
Adrian Kuśmierek <huskyusadoggy@gmail.com>
2022-09-13 10:20:33 +0200

    l

---
+++ opdracht_3.2/main.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Reply!</title>
+</head>
+<body>
+    <?php
+        echo "Hello, " . $_POST["fname"] . " " . $_POST["lname"] . "!<br>";
+    ?>
+</body>
+</html>
